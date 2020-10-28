fcac0f019a9194727f49791449beafac1ac01bbe
marquitakelder <57427961+marquitakelder@users.noreply.github.com>
2020-10-27 23:02:32 -0400

    changes

---
+++ script.js
@@ -0,0 +1,54 @@
+// Step 2
+{
+    let name = "Marquita";
+    console.log(name); 
+}
+
+// Step 3
+{
+    let name = "Jones";
+    console.log(name);
+}
+
+// Step 4
+const name = "Marquita";
+console.log(name);
+
+// Steps 5 and 6
+if(1 + 1 === 2){
+    var number = 4;
+    console.log(number);
+}
+
+// Steps 7 and 8
+if(2 - 2 === 0){
+    let number = 0;
+    console.log(number);
+}
+
+// Step 9
+// const name = "Jones";
+
+// Step 10
+const array = [];
+
+// Step 11
+array.push(1, 2, 3);
+console.log(array);
+
+// Step 12
+const more = {
+    
+}
+
+// Step 13
+const Me = {
+    name: "Marquita",
+    age: "34",
+};
+console.log(Me);
+
+// Step 14
+console.log(`Hello I am ${Me.name}.`);
+
+// Step 15

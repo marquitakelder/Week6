d77a5c94cd913f9a6d493827de9bfd70e411527e
marquitakelder <57427961+marquitakelder@users.noreply.github.com>
2020-10-29 18:26:27 -0400

    changes

---
+++ JavaScript Notes.js
@@ -0,0 +1,56 @@
+// JavaScript = Created for client side manipulation of the DOM; If it has a browser, it can run JavaScript;
+
+// Inner vs External
+// JavaScript relies on elements in the DOM for it to operate optimally, thus needs to run last; Place before the closing body tag or after the meta information with a defer tag
+
+// Property = Something that describes an object; a key-value pair
+
+// Methods = A function that belongs to an object;
+
+// Parameters = Values passed to a method or function
+
+// Arrays 
+const arr = [1, ['a', 'b', 'c'], 2, 3];
+console.log(arr[1][2]); 
+
+    // Multidimensional Arrays
+    // Use square brackets for each element of an array within an array
+    const arr = [1, [true, false], 2, 3];
+    console.log(arr[1]);
+
+    const arr = ['a', 'b', [1, 2, {property: 'value', key: 'value'}], true, [10, 55]]; 
+    // Get "true"
+    console.log(arr[3]);
+    // Get "10"
+    console.log(arr[4][0]);
+
+    // .indexOf() = returns the index of an array element
+
+    // .push() = pushes something onto the end of an array
+
+    // .pop() = removes something from the end of an array
+
+    // .unshift() = pushes something onto the beginning of an array
+
+    // .shift() = removes the first element from the array
+
+    // .concat() = combines arrays 
+
+    // .splice() = modifies elements in an array; modifies the original array
+
+    // .slice() = doesn't modify the original array
+
+// Strings
+var str1 = 'hello';
+var str2 = 'world';
+
+var str3 = str1 + ' ' + str2
+
+var str4 = 'Hello ';
+str4 += 'world my name is Harcourt';
+
+console.log(str4); 
+// returns "Hello world my name is Harcourt"
+
+// for Numbers and Strings, += appends elements
+
--- script.js
+++ script.js
@@ -42,13 +42,62 @@ const more = {
 }
 
 // Step 13
-const Me = {
+const me = {
     name: "Marquita",
     age: "34",
 };
-console.log(Me);
+console.log(me);
 
 // Step 14
-console.log(`Hello I am ${Me.name}.`);
+// console.log(`Hello I am ${Me.name}.`);
 
 // Step 15
+let person = {
+    name: "Jones",
+    age: "3",
+    salutation: function () {
+        console.log(`Hello I am ${person.name} and I am ${person.age}.`);
+    }
+};
+
+person.salutation();
+
+// Step 16
+let {name1, age1} = {name1: "Jones", age1: "3"};
+console.log(name1, age1);
+
+// Step 17
+hello = () => {
+    var1 = console.log("Hello World!");
+}
+hello(); 
+
+// Step 18
+hello = (str) => {
+    var arrow2 = console.log(str);
+}
+hello("Hi!");
+
+// Step 19
+let arrow3 = (num1, num2) => {
+    return "You are not allowed to use the return keyword.";
+}
+console.log(arrow3());
+
+// Step 20
+setTimeout (hello = () => {
+    console.log("Hello again!");
+}, 2000) 
+
+// Step 21
+for(var i=0; i < array.length; i++) {
+    console.log(array[i]);
+}
+for(const num of array) {
+    console.log(num);
+}
+
+// Step 22
+for(const key in me) {
+    console.log(`${key}: ${me[key]}`);
+}
